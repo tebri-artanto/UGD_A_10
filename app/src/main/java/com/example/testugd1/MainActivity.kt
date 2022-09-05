3dfa5d6beb69f2bccdedd2d107caf6e241b3b678
tebri-artanto <71914107+tebri-artanto@users.noreply.github.com>
2022-09-05 13:48:37 +0700

    adding masuk ke signup

--- app/src/main/java/com/example/testugd1/MainActivity.kt
+++ app/src/main/java/com/example/testugd1/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.testugd1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
+import com.google.android.material.textview.MaterialTextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainLayout: ConstraintLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSignUp: TextView = findViewById(R.id.tvSignUp)
+
+        btnSignUp.setOnClickListener{
+            val moveSignUp = Intent(this@MainActivity, SignUp::class.java)
+            startActivity(moveSignUp)
+        }
+
     }
 }
