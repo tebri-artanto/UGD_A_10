d1da1d96b5f07971974c2b44b2abed1622fad24e
rosechill <bagasab11@gmail.com>
2022-09-07 17:39:57 +0700

    lucas mau bikin fragment

---
+++ app/src/main/java/com/example/testugd1/bottom_navigation.kt
@@ -0,0 +1,16 @@
+package com.example.testugd1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class bottom_navigation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bottom_navigation)
+
+
+        }
+    }
+
