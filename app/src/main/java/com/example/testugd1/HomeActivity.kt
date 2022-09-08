d0797468e9e9ddb5bb1151f2dc1648af9adc4cdb
uRealGH <111778068+uRealGH@users.noreply.github.com>
2022-09-09 01:54:12 +0700

    navigation button dan fragmentnya

--- app/src/main/java/com/example/testugd1/HomeActivity.kt
+++ app/src/main/java/com/example/testugd1/HomeActivity.kt
@@ -2,10 +2,37 @@ package com.example.testugd1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.testugd1.databinding.ActivityMainBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Search())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId) {
+                R.id.Search -> replaceFragment(Search())
+                R.id.Home -> replaceFragment(Home())
+                R.id.Profile -> replaceFragment(Profile())
+                else ->{
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 }
