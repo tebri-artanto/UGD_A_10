b8491017dbc61b26d1dbf62e43ab0c11875edc12
rosechill <bagasab11@gmail.com>
2022-09-20 16:23:44 +0700

    register view binding

--- app/src/main/java/com/example/testugd1/SignUp.kt
+++ app/src/main/java/com/example/testugd1/SignUp.kt
@@ -6,30 +6,29 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.viewModels
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.get
+import com.example.testugd1.databinding.ActivitySignUpBinding
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputLayout
 import com.google.android.material.textview.MaterialTextView
 
 class SignUp : AppCompatActivity() {
-    private lateinit var inputUsername:TextInputLayout
-    private lateinit var inputPassword: TextInputLayout
-    private lateinit var inputEmail: TextInputLayout
-    private lateinit var inputTangalLahir: TextInputLayout
-    private lateinit var inputNoTelpon: TextInputLayout
-    private lateinit var signUpLayout: ConstraintLayout
+
+    private lateinit var binding: ActivitySignUpBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
-        supportActionBar?.hide()
 
-        inputUsername = findViewById(R.id.inputLayoutUsername)
-        inputPassword = findViewById(R.id.inputLayoutPassword)
-        inputEmail = findViewById(R.id.inputLayoutEmail)
-        inputTangalLahir = findViewById(R.id.inputLayoutTanggalLahir)
-        inputNoTelpon = findViewById(R.id.inputLayoutNoTelpon)
-        signUpLayout = findViewById(R.id.singUpLayout)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
+
+
 
         val btnSignUp2: Button = findViewById(R.id.btnSignUp)
 
@@ -37,35 +36,35 @@ class SignUp : AppCompatActivity() {
             var checkSignUp = false
 
             val mBundle = Bundle()
-            val username: String = inputUsername.getEditText()?.getText().toString()
-            val password: String = inputPassword.getEditText()?.getText().toString()
-            val email: String = inputEmail.getEditText()?.getText().toString()
-            val tanggalLahir: String = inputTangalLahir.getEditText()?.getText().toString()
-            val noTelpon: String = inputNoTelpon.getEditText()?.getText().toString()
+            val username: String = binding.inputLayoutUsername.toString()
+            val password: String = binding.inputLayoutPassword.toString()
+            val email: String = binding.inputLayoutEmail.toString()
+            val tanggalLahir: String = binding.inputLayoutTanggalLahir.toString()
+            val noTelpon: String = binding.inputLayoutNoTelpon.toString()
 
 
             if (username.isEmpty()){
-                inputUsername.setError("Username must be filled with text")
+                binding.inputLayoutUsername.setError("Username must be filled with text")
                 checkSignUp = false
             }
 
             if (password.isEmpty()){
-                inputPassword.setError("Password must be filled with text")
+                binding.inputLayoutPassword.setError("Password must be filled with text")
                 checkSignUp = false
             }
 
             if (email.isEmpty()){
-                inputEmail.setError("Email must be filled with text")
+                binding.inputLayoutEmail.setError("Email must be filled with text")
                 checkSignUp = false
             }
 
             if (tanggalLahir.isEmpty()){
-                inputTangalLahir.setError("Tanggal Lahir must be filled with text")
+                binding.inputLayoutTanggalLahir.setError("Tanggal Lahir must be filled with text")
                 checkSignUp = false
                 }
 
             if (noTelpon.isEmpty()){
-                inputNoTelpon.setError("No Telpon must be filled with text")
+                binding.inputLayoutNoTelpon.setError("No Telpon must be filled with text")
                 checkSignUp = false
             }
             if (username.isNotEmpty() && password.isNotEmpty() && email.isNotEmpty() && tanggalLahir.isNotEmpty() && noTelpon.isNotEmpty()) checkSignUp = true
