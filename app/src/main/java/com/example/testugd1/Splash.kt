7fa1d846b4a38acb33dc66239f5db2f4c9e1e425
uRealGH <111778068+uRealGH@users.noreply.github.com>
2022-09-21 16:00:13 +0700

    splash screen (belum dicoba)

---
+++ app/src/main/java/com/example/testugd1/Splash.kt
@@ -0,0 +1,23 @@
+package com.example.testugd1
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.spalsh_screen)
+
+        supportActionBar?.hide()
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },6000)
+    }
+}
