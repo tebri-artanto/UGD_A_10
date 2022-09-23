b674741b6492cdb0e5455f805732d2a15157e6cc
tebri-artanto <71914107+tebri-artanto@users.noreply.github.com>
2022-09-23 23:56:49 +0700

    hampir selesai

--- app/src/main/java/com/example/testugd1/homeMenu/HomeFragment.kt
+++ app/src/main/java/com/example/testugd1/homeMenu/HomeFragment.kt
@@ -7,11 +7,17 @@ import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
 import com.example.testugd1.EditActivity
 import com.example.testugd1.room.Constant
 import com.example.testugd1.room.User
 import com.example.testugd1.room.UserDB
+import com.example.testugd1.roomTP.Pariwisata
+import com.example.testugd1.roomTP.PariwisataDB
 import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -19,85 +25,101 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 
-class homeFragment : AppCompatActivity() {
-    val db by lazy { UserDB(this) }
-    lateinit var noteAdapter: NoteAdapter
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setupListener()
-        setupRecycleView()
+class homeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    private fun setupRecycleView() {
-        noteAdapter = NoteAdapter(arrayListOf(), object :
-            NoteAdapter.OnAdapterListener{
-            override fun onClick(note: User) {
-                intentEdit(note.id, Constant.TYPE_READ)
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val layoutManager = LinearLayoutManager(context)
+        //val adapter : rvTempatPariwisata = rvTempatPariwisata(TempatPariwisata.litsOfKelas)
+//        val rvPariwisata : RecyclerView = view.findViewById(R.id.rv_tempat_pariwisata)
+//        rvPariwisata.layoutManager = layoutManager
+//        rvPariwisata.adapter = adapter
+    }}
+//    val db by lazy { PariwisataDB(this) }
+//    lateinit var noteAdapter: NoteAdapter
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+////        setupListener()
+////        setupRecycleView()
+//    }
+//}
 
-            override fun onUpdate(note: User) {
-                intentEdit(note.id, Constant.TYPE_UPDATE)
-            }
-
-            override fun onDelete(note: User) {
-                deleteDialog(note)
-            }
-        })
-        rv_tempat_pariwisata.apply {
-            layoutManager = LinearLayoutManager(applicationContext)
-            adapter = noteAdapter
-        }
-    }
-
-    private fun deleteDialog(user: User){
-        val alertDialog = AlertDialog.Builder(this)
-        alertDialog.apply {
-            setTitle("Confirmation")
-            setMessage("Are You Sure to delete this data From${user.username}?")
-            setNegativeButton("Cancel", DialogInterface.OnClickListener
-            { dialogInterface, i ->
-                dialogInterface.dismiss()
-            })
-            setPositiveButton("Delete", DialogInterface.OnClickListener
-            { dialogInterface, i ->
-                dialogInterface.dismiss()
-                CoroutineScope(Dispatchers.IO).launch {
-                    db.userDao().deleteUser(user)
-                    loadData()
-                }
-            })
-        }
-        alertDialog.show()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        loadData()
-    }
-
-    fun loadData() {
-        CoroutineScope(Dispatchers.IO).launch {
-            val notes = db.userDao().getUser(User)
-            Log.d("MainActivity","dbResponse: $notes")
-            withContext(Dispatchers.Main){
-                noteAdapter.setData(notes)
-            }
-        }
-    }
-
-    fun setupListener() {
-        button_create.setOnClickListener{
-            intentEdit(0,Constant.TYPE_CREATE)
-        }
-    }
-
-    fun intentEdit(noteId : Int, intentType: Int) {
-        startActivity(
-            Intent(applicationContext, EditActivity::class.java)
-                .putExtra("intent_id", noteId)
-                .putExtra("intent_type", intentType)
-        )
-    }
-}
+//    private fun setupRecycleView() {
+//        noteAdapter = NoteAdapter(arrayListOf(), object :
+//            NoteAdapter.OnAdapterListener{
+//            override fun onClick(pariwisata: Pariwisata) {
+//                intentEdit(pariwisata.id, Constant.TYPE_READ)
+//            }
+//
+//            override fun onUpdate(pariwisata: Pariwisata) {
+//                intentEdit(pariwisata.id, Constant.TYPE_UPDATE)
+//            }
+//
+//            override fun onDelete(pariwisata: Pariwisata) {
+//                deleteDialog(pariwisata)
+//            }
+//        })
+//        list_note.apply {
+//            layoutManager = LinearLayoutManager(applicationContext)
+//            adapter = noteAdapter
+//        }
+//    }
+//
+//    private fun deleteDialog(pariwisata: Pariwisata){
+//        val alertDialog = AlertDialog.Builder(this)
+//        alertDialog.apply {
+//            setTitle("Confirmation")
+//            setMessage("Are You Sure to delete this data From${user.username}?")
+//            setNegativeButton("Cancel", DialogInterface.OnClickListener
+//            { dialogInterface, i ->
+//                dialogInterface.dismiss()
+//            })
+//            setPositiveButton("Delete", DialogInterface.OnClickListener
+//            { dialogInterface, i ->
+//                dialogInterface.dismiss()
+//                CoroutineScope(Dispatchers.IO).launch {
+//                    db.pariwisataDao().deleteUser(user)
+//                    loadData()
+//                }
+//            })
+//        }
+//        alertDialog.show()
+//    }
+//
+//    override fun onStart() {
+//        super.onStart()
+//        loadData()
+//    }
+//
+//    fun loadData() {
+//        CoroutineScope(Dispatchers.IO).launch {
+//            val notes = db.userDao().getUser(User)
+//            Log.d("MainActivity","dbResponse: $notes")
+//            withContext(Dispatchers.Main){
+//                noteAdapter.setData(notes)
+//            }
+//        }
+//    }
+//
+//    fun setupListener() {
+//        button_create.setOnClickListener{
+//            intentEdit(0,Constant.TYPE_CREATE)
+//        }
+//    }
+//
+//    fun intentEdit(noteId : Int, intentType: Int) {
+//        startActivity(
+//            Intent(applicationContext, EditActivity::class.java)
+//                .putExtra("intent_id", noteId)
+//                .putExtra("intent_type", intentType)
+//        )
+//    }
+//}
