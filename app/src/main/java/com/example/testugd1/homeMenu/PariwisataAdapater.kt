81e99994fcc1b0191a04f60c809e5cd3792a73e3
uRealGH <felixrobertuslucas@gmail.com>
2022-09-23 23:19:06 +0700

    7:52 PM

--- app/src/main/java/com/example/testugd1/homeMenu/PariwisataAdapater.kt
+++ app/src/main/java/com/example/testugd1/homeMenu/PariwisataAdapater.kt
@@ -1,12 +1,57 @@
 package com.example.testugd1.homeMenu
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.example.testugd1.R
+import com.example.testugd1.room.User
+import kotlinx.android.synthetic.main.activity_pariwisata_adapter.view.*
 
-class PariwisataAdapater : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pariwisata_adapter)
+
+class NoteAdapter(private val User: ArrayList<User>, private val
+listener: OnAdapterListener) :
+    RecyclerView.Adapter<NoteAdapter.NoteViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
+            NoteViewHolder {
+        return NoteViewHolder(
+
+            LayoutInflater.from(parent.context).inflate(R.layout.activity_pariwisata_adapter,parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val note = User[position]
+        holder.view.text_title.text = note.username
+        holder.view.text_title.setOnClickListener{
+            listener.onClick(note)
+        }
+        holder.view.icon_pesan.setOnClickListener{
+            listener.onUpdate(note)
+        }
+        holder.view.icon_profil.setOnClickListener{
+            listener.onDelete(note)
+        }
+
+    }
+
+    override fun getItemCount() = User.size
+
+    inner class NoteViewHolder( val view: View) :
+        RecyclerView.ViewHolder(view)
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setData(list: List<User>){
+        User.clear()
+        User.addAll(list)
+        notifyDataSetChanged()
+    }
+
+    interface OnAdapterListener {
+        fun onClick(note: User)
+        fun onUpdate(note: User)
+        fun onDelete(note: User)
     }
 }
