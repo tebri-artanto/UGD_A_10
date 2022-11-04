57dfe7799901c3d56d117f4e1ad0308e4c0652b7
uRealGH <felixrobertuslucas@gmail.com>
2022-11-04 19:13:22 +0700

    Input Camera

--- app/src/main/java/com/example/testugd1/CameraMenu.kt
+++ app/src/main/java/com/example/testugd1/CameraMenu.kt
@@ -1,11 +1,32 @@
 package com.example.testugd1
 
+import android.annotation.SuppressLint
+import android.hardware.Camera
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.ImageButton
 
 class CameraMenu : AppCompatActivity() {
+    private var mCamera: Camera? = null
+    private var mCameraView: CameraView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera_menu)
+        try {
+            mCamera = Camera.open()
+        } catch (e: Exception) {
+            Log.d("error", "Camera eror on SurfaceCreated" + e.message)
+        }
+        if (mCamera != null) {
+            mCameraView = CameraView(this, mCamera!!)
+            val camera_view = findViewById<View>(R.id.FLCamera) as FrameLayout
+            camera_view.addView(mCameraView)
+        }
+        @SuppressLint("MissingInflaredId", "LocalSuppress") val imageClose =
+            findViewById<View>(R.id.imgClose) as ImageButton
+        imageClose.setOnClickListener { view: View? -> System.exit(0) }
     }
 }
