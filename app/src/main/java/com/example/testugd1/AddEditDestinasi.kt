43dd7f624512d5cc11fc9cf4ea9892fc339c280f
tebri-artanto <71914107+tebri-artanto@users.noreply.github.com>
2022-11-15 14:58:49 +0700

    push awalan volley

---
+++ app/src/main/java/com/example/testugd1/AddEditDestinasi.kt
@@ -0,0 +1,11 @@
+package com.example.testugd1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddEditDestinasi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_edit_destinasi)
+    }
+}
