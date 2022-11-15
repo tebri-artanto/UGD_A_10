463ee7679130f040c9ad486fe7ed093078cafd76
tebri-artanto <71914107+tebri-artanto@users.noreply.github.com>
2022-11-15 15:06:53 +0700

    nambah api dan model dan gradle

---
+++ app/src/main/java/com/example/testugd1/api/DestinasiApi.kt
@@ -0,0 +1,15 @@
+package com.example.gd11_a_0680.api
+
+class DestinasiApi {
+    companion object {
+        val BASE_URL = "https://637332600bb6b698b604b3b5.mockapi.io/api/"
+
+
+        val GET_ALL_URL = BASE_URL + "destinasi/"
+        val GET_BY_ID_URL = BASE_URL + "destinasi/"
+        val ADD_URL = BASE_URL + "destinasi"
+        val UPDATE_URL = BASE_URL + "destinasi/"
+        val DELETE_URL = BASE_URL + "destinasi/"
+    }
+
+}
---
+++ app/src/main/java/com/example/testugd1/models/Destinasi.kt
@@ -0,0 +1,5 @@
+package com.example.gd11_a_0680.models
+
+class Destinasi (var nama: String, var lokasi: String, var harga: String, var tipe: String) {
+    var id: Long? = null
+}
